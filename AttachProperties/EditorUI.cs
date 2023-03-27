98851cdd595d53a479d487cfed19eca8bb68390a
Friday B. akpan <friday.b.akpan@svr.cyphercrescent.com>
2023-03-27 08:44:35 +0100

    Tested Attached properties

---
+++ AttachProperties/EditorUI.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Automation.Peers;
+using System.Windows.Media;
+
+namespace DP_Demo.AttachProperties
+{
+    public class EditorUI : UIElement
+    {
+        public static int GetZoom(DependencyObject obj)
+        {
+            return (int)obj.GetValue(ZoomProperty);
+        }
+
+        public static void SetZoom(DependencyObject obj, int value)
+        {
+            obj.SetValue(ZoomProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ZoomProperty =
+            DependencyProperty.RegisterAttached("Zoom", typeof(int), typeof(EditorUI), new PropertyMetadata(0));
+
+
+        public EditorUI()
+        {
+
+        }
+
+        protected override AutomationPeer OnCreateAutomationPeer()
+        {
+            return base.OnCreateAutomationPeer();
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+        }
+    }
+}
